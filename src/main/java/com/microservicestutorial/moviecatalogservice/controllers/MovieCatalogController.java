6127a387b1aafc24d152b6890cdc3b315c022f3d
Mickael Thomere <mickael.thomere@gmail.com>
2021-12-11 22:07:01 +0100

    MicroServices Tutorial : call the movie info API via RestTemplate + hardcoded rates

--- src/main/java/com/microservicestutorial/moviecatalogservice/controllers/MovieCatalogController.java
+++ src/main/java/com/microservicestutorial/moviecatalogservice/controllers/MovieCatalogController.java
@@ -1,14 +1,19 @@
 package com.microservicestutorial.moviecatalogservice.controllers;
 
 import com.microservicestutorial.moviecatalogservice.resources.MovieCatalogResource;
+import com.microservicestutorial.moviecatalogservice.resources.MovieInfoResource;
 import com.microservicestutorial.moviecatalogservice.resources.MovieRatingResource;
+import com.microservicestutorial.moviecatalogservice.resources.RatingResource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/catalog")
@@ -17,11 +22,21 @@ public class MovieCatalogController {
     @GetMapping(value = "/{id}")
     public MovieCatalogResource getMoviesById(@PathVariable(value = "id") String userId) {
 
-        List<MovieRatingResource> movies = new ArrayList<>();
-        movies.add(new MovieRatingResource(0, 5, "Minority Report", "sci fi film with Tom Cruise"));
-        movies.add(new MovieRatingResource(1, 5, "Le seigneur des anneaux", "fantasy film based on the book  of Tolkien"));
-        movies.add(new MovieRatingResource(2, 3, "Titanic", "romance film wih Leonoardo Di Caprio and Kate Winslet"));
-        movies.add(new MovieRatingResource(0, 5, "La ligne verte", "fiction film with Tom Hanks"));
+        RestTemplate restTemplate = new RestTemplate();
+
+        List<RatingResource> moviesRates = Arrays.asList(
+                new RatingResource(0, 4),
+                new RatingResource(1, 3),
+                new RatingResource(2, 5)
+        );
+
+        List<MovieRatingResource> movies = moviesRates.stream()
+                .map(moviesRate -> {
+                    MovieInfoResource movieInfoResource = restTemplate.getForObject("http://localhost:8081/movie/" + moviesRate.getMovieId(), MovieInfoResource.class);
+                    return new MovieRatingResource(movieInfoResource.getId(), moviesRate.getRate(), movieInfoResource.getName(), movieInfoResource.getDescription());
+                })
+                .collect(Collectors.toList());
+
 
         return new MovieCatalogResource(userId, "Mika T", movies);
     }
