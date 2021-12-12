beae51973fee53a4fc63670a65adab0a29683b14
Mickael Thomere <mickael.thomere@gmail.com>
2021-12-12 19:39:23 +0100

    Microservices Tutorial : add load balancing + access microservices in RestTemplate via eureka url

--- src/main/java/com/microservicestutorial/moviecatalogservice/controllers/MovieCatalogController.java
+++ src/main/java/com/microservicestutorial/moviecatalogservice/controllers/MovieCatalogController.java
@@ -1,8 +1,11 @@
 package com.microservicestutorial.moviecatalogservice.controllers;
 
-import com.fasterxml.jackson.annotation.JacksonInject;
-import com.microservicestutorial.moviecatalogservice.resources.*;
+import com.microservicestutorial.moviecatalogservice.resources.MovieCatalogResource;
+import com.microservicestutorial.moviecatalogservice.resources.MovieInfoResource;
+import com.microservicestutorial.moviecatalogservice.resources.MovieRatingResource;
+import com.microservicestutorial.moviecatalogservice.resources.UserRatingsResource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,14 +13,15 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/catalog")
 public class MovieCatalogController {
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
 
     @Autowired
     private RestTemplate restTemplate;
@@ -28,16 +32,17 @@ public class MovieCatalogController {
     @GetMapping(value = "/{id}")
     public MovieCatalogResource getMoviesById(@PathVariable(value = "id") String userId) {
 
-        UserRatingsResource userRatingsResource = restTemplate.getForObject("http://localhost:8083/movie-rating/user/" + userId, UserRatingsResource.class);
+        UserRatingsResource userRatingsResource = restTemplate.getForObject("http://movie-ratings-api/movie-rating/user/" + userId, UserRatingsResource.class);
 
         List<MovieRatingResource> movies = userRatingsResource.getRatings().stream()
                 .map(moviesRate -> {
-                    MovieInfoResource movieInfoResource = restTemplate.getForObject("http://localhost:8081/movie/" + moviesRate.getMovieId(), MovieInfoResource.class);
+                    MovieInfoResource movieInfoResource = restTemplate.getForObject("http://movie-info-api/movie/" + moviesRate.getMovieId(), MovieInfoResource.class);
                     return new MovieRatingResource(movieInfoResource.getId(), moviesRate.getRate(), movieInfoResource.getName(), movieInfoResource.getDescription());
                 })
                 .collect(Collectors.toList());
 
-
+        System.out.println("instances : " + discoveryClient.getInstances("movie-ratings-api"));
+        System.out.println("services : " + discoveryClient.getServices());
         return new MovieCatalogResource(userId, "Mika T", movies);
     }
 }
